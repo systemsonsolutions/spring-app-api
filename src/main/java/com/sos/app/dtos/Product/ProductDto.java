fef036523b18c825072ca04da3198d2ebb2f4fd4
Israel <israel_alves77@hotmail.com>
2024-11-17 11:01:32 -0300

    hotfix: Correção do ProductDto.

--- src/main/java/com/sos/app/dtos/Product/ProductDto.java
+++ src/main/java/com/sos/app/dtos/Product/ProductDto.java
@@ -1,7 +1,5 @@
 package com.sos.app.dtos.Product;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
